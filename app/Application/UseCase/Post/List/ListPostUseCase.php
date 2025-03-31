67efe8820e3d215821bca5cecd0022e53d8a5a8e
haaragard <haaragard@gmail.com>
2025-03-31 20:01:27 -0300

    feat: add cache for posts list

--- app/Application/UseCase/Post/List/ListPostUseCase.php
+++ app/Application/UseCase/Post/List/ListPostUseCase.php
@@ -4,24 +4,69 @@ declare(strict_types=1);
 
 namespace App\Application\UseCase\Post\List;
 
+use App\_Shared\Utils\Collection;
+use App\Domain\Post\Entity\PostEntityInterface;
+use App\Infrastructure\Cache\CacheInterface;
 use App\Infrastructure\Persistence\Mysql\Repository\Post\Get\PaginateDto;
 use App\Infrastructure\Persistence\Mysql\Repository\Post\PostRepositoryInterface;
+use function Hyperf\Config\config;
 
 class ListPostUseCase
 {
     public function __construct(
-        private PostRepositoryInterface $postRepository
+        private PostRepositoryInterface $postRepository,
+        private CacheInterface $cache
     ) {
     }
 
     public function execute(ListPostInputDto $input): ListPostOutputDto
     {
-        $paginateDto = new PaginateDto(
+        $posts = $this->getFromCache(
             page: $input->page,
             perPage: $input->perPage
         );
-        $entities = $this->postRepository->paginate($paginateDto);
 
-        return new ListPostOutputDto($entities);
+        return new ListPostOutputDto($posts);
+    }
+
+    private function getCacheKey(int $page, int $perPage): string
+    {
+        return sprintf(
+            config('cache.keys.post.list'),
+            $page,
+            $perPage
+        );
+    }
+
+    /**
+     * @return Collection<PostEntityInterface>
+     */
+    private function getPosts(int $page, int $perPage): Collection
+    {
+        $paginateDto = new PaginateDto(
+            page: $page,
+            perPage: $perPage
+        );
+
+        return $this->postRepository->paginate($paginateDto);
+    }
+
+    private function getFromCache(int $page, int $perPage): Collection
+    {
+        $cacheKey = $this->getCacheKey(
+            page: $page,
+            perPage: $perPage
+        );
+
+        $postsFromCache = $this->cache->get(
+            $cacheKey,
+            fn (): array => $this->getPosts($page, $perPage)->toArray()
+        );
+
+        if ($postsFromCache instanceof \Closure) {
+            $postsFromCache = $postsFromCache();
+        }
+
+        return new Collection($postsFromCache);
     }
 }
