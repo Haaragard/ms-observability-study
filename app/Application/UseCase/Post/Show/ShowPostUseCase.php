c2f7f09ee3ea3732993bdb447d49e0c01a7be086
haaragard <haaragard@gmail.com>
2025-03-31 20:05:59 -0300

    feat: add cache for posts show

--- app/Application/UseCase/Post/Show/ShowPostUseCase.php
+++ app/Application/UseCase/Post/Show/ShowPostUseCase.php
@@ -5,12 +5,16 @@ declare(strict_types=1);
 namespace App\Application\UseCase\Post\Show;
 
 use App\Application\Exception\_Shared\EntityNotFoundException;
+use App\Domain\Post\Entity\PostEntityInterface;
+use App\Infrastructure\Cache\CacheInterface;
 use App\Infrastructure\Persistence\Mysql\Repository\Post\PostRepositoryInterface;
+use function Hyperf\Config\config;
 
 class ShowPostUseCase
 {
     public function __construct(
-        private PostRepositoryInterface $postRepository
+        private PostRepositoryInterface $postRepository,
+        private CacheInterface $cache
     ) {
     }
 
@@ -19,11 +23,35 @@ class ShowPostUseCase
      */
     public function execute(ShowPostInputDto $input): ShowPostOutputDto
     {
-        $postEntity = $this->postRepository->find($input->id);
+        $postEntity = $this->getPost($input->id);
         if (is_null($postEntity)) {
             throw new EntityNotFoundException('Post not found');
         }
 
         return new ShowPostOutputDto(postEntity: $postEntity);
+    }
+
+    private function getCacheKey(int $id): string
+    {
+        return sprintf(
+            config('cache.keys.post.show'),
+            $id
+        );
+    }
+
+    private function getPost(int $id): ?PostEntityInterface
+    {
+        $cacheKey = $this->getCacheKey($id);
+
+        $postFromCache = $this->cache->get(
+            $cacheKey,
+            fn (): ?PostEntityInterface => $this->postRepository->find($id)
+        );
+
+        if ($postFromCache instanceof \Closure) {
+            $postFromCache = $postFromCache();
+        }
+
+        return $postFromCache;
     }
 }
