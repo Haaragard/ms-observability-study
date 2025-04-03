414ad6cf2f584b8342e5ac0971e55f339ef8bace
haaragard <haaragard@gmail.com>
2025-04-02 22:35:15 -0300

    temp: add new route with trace & log using OTEL

--- config/routes.php
+++ config/routes.php
@@ -21,3 +21,32 @@ Router::addGroup('/posts', static function (): void {
     Router::post('', StorePostController::class);
     Router::get('/{id}', ShowPostController::class);
 });
+
+$tracer = \OpenTelemetry\API\Globals::tracerProvider()->getTracer('demo');
+$loggerProvider = \OpenTelemetry\API\Globals::loggerProvider();
+$monologHandler = new \OpenTelemetry\Contrib\Logs\Monolog\Handler(
+    $loggerProvider,
+    \Psr\Log\LogLevel::INFO
+);
+$monolog = new \Monolog\Logger('otel-php-monolog', [$monologHandler]);
+
+Router::get(
+    '/rolldice',
+    function (
+        \Hyperf\HttpServer\Contract\RequestInterface $request,
+        \Hyperf\HttpServer\Contract\ResponseInterface $response
+    ) use (
+        $tracer,
+        $monolog
+    ) {
+    $span = $tracer->spanBuilder('manual-span')
+        ->startSpan();
+    $rollDiceResult = random_int(1, 6);
+
+    $monolog->info('rolled dice', ['result' => $rollDiceResult]);
+
+    $span->addEvent('rolled dice', ['result' => $rollDiceResult])
+        ->end();
+
+    return $response->json(['result' => $rollDiceResult]);
+});
